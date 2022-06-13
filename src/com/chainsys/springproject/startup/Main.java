b0e8a48ddb9753eb65ed3b1090a72f6d53a12a1b
Maheshwaran5 <maheshwaran.nagarathinam@chainsys.com>
2022-06-13 17:59:43 +0530

    update projecta

--- src/com/chainsys/springproject/startup/Main.java
+++ src/com/chainsys/springproject/startup/Main.java
@@ -1,35 +1,50 @@
 package com.chainsys.springproject.startup;
 
-import com.chainsys.springproject.test.TestAnnotationConfig;
-import com.chainsys.springproject.test.TestCar;
-import com.chainsys.springproject.test.TestClasspathXml;
-import com.chainsys.springproject.test.TestXmlBeanFactory;
 
+import java.util.Scanner;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import com.chainsys.springproject.view.EmployeeSprings;
+import com.chainsys.springproject.test.TestClasspathXml;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-//		TestAnnotationConfig.testA();
-//		TestXmlBeanFactory.testA();
-		TestClasspathXml.testLunchFactory();
-		//TestCar.firsttest();
+	
+	employeeMenu();
+	}
+	public static void employeeMenu() {
+		ConfigurableApplicationContext apcontext=new ClassPathXmlApplicationContext("Employee.xml");
+		EmployeeSprings empspring = apcontext.getBean(EmployeeSprings.class);
+		Scanner sc=new Scanner(System.in);
+		empspring.setApcontext(apcontext);
+		empspring.setSc(sc);
+		System.out.println("1.) To add New employee in to table");
+		System.out.println("2.) To Get employee datails by id from table");
+		System.out.println("3.) To Update employee details");
+		System.out.println("4.) To Delete Employee details");
+		int vCase=sc.nextInt();
+		switch(vCase) {
+		case 1://no sir
+			empspring.addNewEmployee();
+			break;
+		case 2:
+			empspring.GetEmplyeeById();
+			break;
+		case 3:
+			empspring.UpdateEmployee();
+			break;
+		case 4:
+			empspring.DeleteEmployee();
+			break;
+		default:
+			System.out.println("enter valid number");
+		}
+		apcontext.close();
+		sc.close();
 	
 	}
-
 }
-
-
-// First JVM gets loaded
-// main method gets executed
-// ApplicationContent Is loaded
-// ApplicationContent reads xml configuration file
-// ApplicationContent pre-loads one object for each bean in the config file if the bean is not marked as lazy-init="true"
-// Lazy-init="true" this informs springContainer not to pre-loaded ogbject for this bean
-// By default springs early loads object in memory.
-// For lazy-init beans objects is created only when the getBean() method is called
-// For pre-init beans  getBean() returns the reference of the pre-loaded objects.
-// By default only one object for every bean is created(singleton)
-// singleton: Atleast one object for every class but not more than one object
-// Spring for lazy-init beans creates a singleton objects the first call of getBeans() method and then
-// users that objects for future getBean()  method calls.
--- src/com/chainsys/springproject/test/TestClasspathXml.java
+++ src/com/chainsys/springproject/test/TestClasspathXml.java
@@ -1,127 +1,149 @@
 package com.chainsys.springproject.test;
 
-import org.springframework.context.ApplicationContext; //parent
+import org.springframework.context.ApplicationContext;//parent
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext; //child
+import org.springframework.context.support.ClassPathXmlApplicationContext;//child
 
+import com.chainsys.springproject.autowire.Car;
+import com.chainsys.springproject.autowire.CarServices;
 import com.chainsys.springproject.beans.Actor;
 import com.chainsys.springproject.beans.Calendar;
 import com.chainsys.springproject.beans.Customer;
 import com.chainsys.springproject.beans.Employee;
 import com.chainsys.springproject.beans.Lunch;
-import com.chainsys.springproject.beans.Movie;
-
 import com.chainsys.springproject.beans.Scoreboard;
 import com.chainsys.springproject.lifecycle.AnnotationLc;
 import com.chainsys.springproject.lifecycle.InitDesposeBean;
 import com.chainsys.springproject.lifecycle.LifeCycleBean;
 public class TestClasspathXml {
-	public static void testA() {
-    	Employee emp=new Employee();
-    	emp.setId(101);
-    	emp.setName("Maheshwaran");
-    	emp.print();
-    }
-    public static void testB() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-    	//Employee emp=apcontext.getBean(Employee.class);
-    	Employee emp=(Employee)apcontext.getBean("emp");
-    	emp.setId(101);
-    	emp.setName("Maheshwaran");
-    	emp.print();
-    	Customer c1=(Customer)apcontext.getBean("c1");
-    	c1.setId(324);
-    	c1.setName("Ayyanar");
-    	c1.print();
-    }
-    public static void testLazy_init() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-    	// when we use lazy init into that class in beans.xml 
-    	// it does not create new object it will use always one object reference
-    	// otherwise when we call more times AppContext it will create new object  
-    	ApplicationContext apcontext2=new ClassPathXmlApplicationContext("beans.xml");
-    	Employee emp=(Employee)apcontext.getBean("emp");
-    	Employee emp2=(Employee)apcontext.getBean("emp");
-    	//how many times getbean is called is not matter it will always use single object reference
-    	Customer c1=(Customer)apcontext.getBean("c1");
-    	Customer c2=(Customer)apcontext.getBean("c1");
-    	System.out.println(emp.hashCode());
-    	System.out.println(emp2.hashCode());
-    	System.out.println(c1.hashCode());
-    	System.out.println(c2.hashCode());
-    	
-    	// why it is create only one object even getbeans method calls more than 1 time?
-    }
-    public static void testPrototype() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-//    	Actor hero=(Actor)apcontext.getBean("act");
-//    	Actor heroine=(Actor)apcontext.getBean("act");
-//    	Actor comedian=(Actor)apcontext.getBean("act");
-//    	Actor villan=(Actor)apcontext.getBean("act");
-////    	System.out.println("");
-    }
-    public static void testBeanWithConstructors() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-    }
-    public static void testFactory() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-    	Movie m= new Movie("samayapuram","pri");
-    	Scoreboard sb1=(Scoreboard)apcontext.getBean("sb1",Scoreboard.class);
-    	Scoreboard sb2=(Scoreboard)apcontext.getBean("sb2",Scoreboard.class);
-    	sb1.targetScore=183;
-    	System.out.println(sb1.targetScore);
-    	
-    }
-    public static void testcalenderFactory() {
-    	ApplicationContext apcontext=new ClassPathXmlApplicationContext("beans.xml");
-    	Calendar today=apcontext.getBean (Calendar.class); today.day=9;
-    	today.month="june";
-    	today.year=2022;
-    	System.out.println(today.day);
-    	System.out.println(today.month);
-    	System.out.println(today.year);
-    }
-    public static void testLunchFactory() { 
-        ApplicationContext ac = new ClassPathXmlApplicationContext("Lunch.xml"); 
-        Lunch nilunch=ac.getBean("nilunch", Lunch.class); 
-        Lunch chlunch=ac.getBean("chlunch", Lunch.class);
-        Lunch nvsilunch=ac.getBean("nvsilunch", Lunch.class);
-        Lunch vsilunch=ac.getBean("vsilunch", Lunch.class);
-        System.out.println("-----");
-        nvsilunch.serve(); 
-        System.out.println("-----");
-        vsilunch.serve(); 
-        System.out.println("-----");
-        nilunch.serve(); 
-        System.out.println("-----");
-        chlunch.serve();
-        }
-    public static void testSetterDi() {
-        ApplicationContext ac1 = new ClassPathXmlApplicationContext("beans.xml");
-        Employee emp = ac1.getBean("emp2",Employee.class);
-    }
-    public static void testLifeCycle() {
-    	 ConfigurableApplicationContext ac1 = new ClassPathXmlApplicationContext("Lc.xml");
-    	 LifeCycleBean life= ac1.getBean(LifeCycleBean.class);
-    	 life.print();
-    	 life=null;
-//    	 System.gc();
-    	 ac1.close();
-    	 ac1=null;
-    	
-    }
-    public static void testInitDestroyBean() {
-    	ConfigurableApplicationContext ac1 = new ClassPathXmlApplicationContext("Lc.xml");
-    	InitDesposeBean init=ac1.getBean(InitDesposeBean.class);
-    	init.print();
-    	
-    }
-    public static void testAnnotationLc() {
-    	ConfigurableApplicationContext ac1 = new ClassPathXmlApplicationContext("Lc.xml");
-    	AnnotationLc ann=ac1.getBean(AnnotationLc.class);
-    	ann.print();
-    	ann=null;
-    	ac1.close();
-    }
+	public static void testA()
+	{
+		Employee emp=new Employee();
+		emp.setId(100);
+		emp.setName("Berlin");
+		emp.print();
+	}
+	public static void testB()
+	{
+		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+//		Employee emp= ac.getBean(Employee.class);
+		Employee emp=(Employee)ac.getBean("emp");
+		emp.setId(100);
+		emp.setName("MaxWell");
+		emp.print();
+		Customer c=ac.getBean(Customer.class);
+		c.setId(17);
+		c.setName("Abde_villiers");
+		c.print();
+	}
+	public static void testlazyinit()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+		Employee emp=(Employee)ac1.getBean("emp");
+		Customer c=ac1.getBean(Customer.class);
+		//lazy-init ="true" for customer. An object is created now (firstcall to getBean() method for Customer.class)
+		Employee secondEmp=(Employee)ac1.getBean("emp");
+		Customer secondCus=ac1.getBean(Customer.class);
+		System.out.println(emp.hashCode());
+		System.out.println(secondEmp.hashCode());
+		System.out.println(c.hashCode());
+		System.out.println(secondCus.hashCode());
+	}	
+	public static void testPrototype()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+		Actor hero=ac1.getBean(Actor.class);
+		Actor heroin=ac1.getBean(Actor.class);
+		Actor comedian=ac1.getBean(Actor.class);
+		Actor friend=ac1.getBean(Actor.class);
+		System.out.println(hero.hashCode());
+		System.out.println(heroin.hashCode());
+		System.out.println(comedian.hashCode());
+		System.out.println(friend.hashCode());
+	}
+	public static void testBeanWithConstructor()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+	}
+	public static void testFactoryMethod()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+		Scoreboard sb=ac1.getBean("sb1",Scoreboard.class);
+		sb.targetScore=183;
+		System.out.println(sb.targetScore);
+		Scoreboard sb2=ac1.getBean("sb2",Scoreboard.class);
+		sb2.targetScore=183;
+		System.out.println(sb2.targetScore);
+	}
+	public static void testCalenderFactory()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+		Calendar today=ac1.getBean(Calendar.class);
+		today.day=9;
+		today.year=2022;
+		today.month="june";
+		System.out.println(today.day);
+		System.out.println(today.year);
+		System.out.println(today.month);
+	}
+	public static void testLunchFactory()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("Lunch.xml");
+		Lunch nvsilunch=ac1.getBean("nvsilunch",Lunch.class);
+		Lunch vsilunch=ac1.getBean("vsilunch",Lunch.class);
+		Lunch nilunch=ac1.getBean("nilunch",Lunch.class);
+		Lunch chilunch=ac1.getBean("chilunch",Lunch.class);
+		System.out.println("-----------");
+		nvsilunch.serve();
+		System.out.println("-----------");
+		nilunch.serve();
+		System.out.println("-----------");
+		vsilunch.serve();
+		System.out.println("-----------");
+		chilunch.serve();
+	}
+	public static void testSetterDi()
+	{
+		ApplicationContext ac1=new ClassPathXmlApplicationContext("beans.xml");
+		Employee emp=ac1.getBean("emp2",Employee.class);
+		emp.print();
+	}
+	public static void testLifeCycle()
+	{
+		ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("Lc.xml");
+		LifeCycleBean life=ac.getBean(LifeCycleBean.class);
+		life.print();
+		life=null;
+		ac.close();
+	}
+	public static void testInitDestroyBean()
+	{
+		ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("Lc.xml");
+		InitDesposeBean init=ac.getBean(InitDesposeBean.class);
+		init.print();
+	}
+	public static void testAnnotationLc()
+	{
+		ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("Lc.xml");
+		AnnotationLc ann=ac.getBean(AnnotationLc.class);
+		ann.print();
+		ann=null;
+		ac.close();
+	}
+	public static void testAutowire()
+	{
+		ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("Autowire.xml");
+		Car car=ac.getBean("car",Car.class);
+		car.start();
+		car.move();
+		car.begin();
+		ac.close();
+	}
+	public static void testAutowireQualifier()
+	{
+		ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("Autowire.xml");
+		CarServices cs=ac.getBean(CarServices.class);
+		cs.startTrip();
+	}
 	
 }
